9e69d855c488b4ef86b2aa7a714db1730700e734
Lucas Lermagne <60604364+LucasLermagne@users.noreply.github.com>
2021-04-15 17:28:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,18 +9,18 @@ const prefix = "!";
 
 client.on("ready",()=>{
     console.log("bot opérationnel");
-    console.log("nb serveurs : " +client.guilds.size);
-    
 
-    for(var i= 0; i < client.guilds.size; i++)
-    {
-        console.log("serveur numero: " + i);
-       // console.log("serveur name: " + client.guilds);    
-    }
     
-    client.guilds.forEach(server => {
-        console.log(server.name + " (id: " + server.id + ")");
-    });
+    client.guilds.forEach(guild => {
+        console.log(`${guild.name} | ${guild.id}`);
+            guild.channels.forEach(channel => {
+            if(channel.name == "free-trading-signals"){
+                console.log(`${channel.name} | ${channel.id}`);
+                channel.send("Achete .....");
+            }
+            });
+        
+      });
 
 
 });
@@ -29,19 +29,6 @@ client.on("message",msg =>{
     if(msg.author.bot) return;
     if(msg.content == prefix + "ping"){
        msg.channel.send(" pong");
-
-        
-
-
-        client.guilds.forEach(guild => {
-        console.log(`${guild.name} | ${guild.id}`);
-            guild.channels.forEach(channel => {
-            if(channel.name == "anonces"){
-                console.log(`${channel.name} | ${channel.id}`);
-            }
-            });
-        
-      });
 
     }
 });
