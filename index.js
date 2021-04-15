fd86e1bfd3f8183e0f0d40db52d651f0309c883a
Lucas Lermagne <60604364+LucasLermagne@users.noreply.github.com>
2021-04-15 17:59:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const client = new Discord.Client;
 
 const prefix = "!";
 
+const msgCustom = new Discord.MessageEmbed()
+.setColor("red")
+.setTitle("1INCH/USDT")
+.addField("titre","contenue");
 
 
 client.on("ready",()=>{
@@ -15,12 +19,7 @@ client.on("ready",()=>{
             guild.channels.forEach(channel => {
             if(channel.name == "free-trading-signals"){
                 channel.send("FLMUSDT	VOLUM>5-10%	BUY>0,85-0,75	SL>0,65	TP>1,35-1,85");
-
-                const msg = new Discord.MessageEmbed()
-                .setColor("red")
-                .setTitle("1INCH/USDT")
-                .addField("titre","contenue");
-                channel.send(msg);
+                channel.send(msgCustom);
                 
             }
             });
