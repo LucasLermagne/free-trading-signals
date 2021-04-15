50ab6fd02c6007db05ab5dc2475659e3cc011ead
Lucas Lermagne <60604364+LucasLermagne@users.noreply.github.com>
2021-04-15 17:19:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ client.on("ready",()=>{
 
     for(var i= 0; i < client.guilds.size; i++)
     {
-        console.log("serveur numero: " + i);  
+        console.log("serveur numero: " + i);
+        console.log("serveur name: " + client.guilds.cache.get(i));    
     }
     
 
