a660c8d6b7cb64b41fce89b68cb655c50a9626cb
Lucas Lermagne <60604364+LucasLermagne@users.noreply.github.com>
2021-04-16 15:31:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,10 +24,23 @@ client.on("message",msg =>{
     }
 
     if(msg.content == prefix + "test"){
-        let embedmsg = new Discord.MessageEmbed().setColor("Red")
-        .setTitle("titre")
-        .setDescription("Description")
-        .setFooter("footer")
+        let embedmsg = new Discord.MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle('Some title')
+        .setURL('https://discord.js.org/')
+        .setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+        .setDescription('Some description here')
+        .setThumbnail('https://i.imgur.com/wSTFkRM.png')
+        .addFields(
+            { name: 'Regular field title', value: 'Some value here' },
+            { name: '\u200B', value: '\u200B' },
+            { name: 'Inline field title', value: 'Some value here', inline: true },
+            { name: 'Inline field title', value: 'Some value here', inline: true },
+        )
+        .addField('Inline field title', 'Some value here', true)
+        .setImage('https://i.imgur.com/wSTFkRM.png')
+        .setTimestamp()
+        .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
         msg.channel.send(embedmsg);
         
  
