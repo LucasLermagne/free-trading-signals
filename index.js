5b20db5a1774969472d20fa28381937c628c7d09
Lucas Lermagne <60604364+LucasLermagne@users.noreply.github.com>
2021-04-16 09:35:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const Discord = require("discord.js");
-const MessageEmbed = require("discord.js");
+
 const client = new Discord.Client;
 
 
 const prefix = "!";
 
-let msgCustom = new MessageEmbed();
-msgCustom.setColor("red");
-msgCustom.setTitle("dfg");
 
 
 
@@ -18,8 +15,7 @@ client.on("ready",()=>{
     client.guilds.forEach(guild => {
             guild.channels.forEach(channel => {
             if(channel.name == "free-trading-signals"){
-                channel.send("FLMUSDT	VOLUM>5-10%	BUY>0,85-0,75	SL>0,65	TP>1,35-1,85");
-                channel.send(msgCustom);
+                channel.send("16-03-2021 IOSTUSDT   SIZE> 5-15%    BUY LEVEL >0,072-0,065    SL>0,059    TP>0,95-0,12");
                 
             }
             });
