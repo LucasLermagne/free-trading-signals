4475f4f0d187fd3df0af06b37899d397000b98da
Lucas Lermagne <60604364+LucasLermagne@users.noreply.github.com>
2021-04-16 15:50:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -45,15 +45,18 @@ client.on("message",msg =>{
         msg.channel.send(embedmsg);*/
 
         const embed = new Discord.RichEmbed()
-        .setTitle("This is your title, it can hold 256 characters")
-        .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
+        .setTitle("New FREE signal")
         /*
         * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
         */
         
         .setColor(0x00AE86)
         .setDescription("This is the main body of text, it can hold 2048 characters.")
-        .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
+        .setTitle("Date")
+        .setDescription("10-02-221")
+        .setTitle("Crypto")
+        .setDescription("USDT/USDT")
+        .setFooter("I authorise the sharing of our trading signals", "http://i.imgur.com/w1vhFSR.png")
         .setImage("http://i.imgur.com/yVpymuV.png")
         .setThumbnail("http://i.imgur.com/p2qNFag.png")
         /*
