54ff324460d734a529d4a8da4d00d8908ab66208
Lucas Lermagne <60604364+LucasLermagne@users.noreply.github.com>
2021-04-16 15:57:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -65,12 +65,20 @@ client.on("message",msg =>{
         /*
         * Inline fields may not display as inline if the thumbnail and/or image is too big.
         */
-        .addField("DATE", "16-03-2021", true)
+        .addField(":calendar:DATE", "16-03-2021", true)
         /*
         * Blank field, useful to create some space.
         */
         .addBlankField(true)
-        .addField("CRYPTO", "IOST/USDT", true);
+        .addField(":sparkles:CRYPTO", "IOST/USDT", true)
+
+        .addField(":dollar:SIZE", "5-15%", true)
+        /*
+        * Blank field, useful to create some space.
+        */
+        .addBlankField(true)
+        .addField(":shopping_cart:BUY LEVEL ", "02,25-1,9", true);
+        
         
         msg.channel.send({embed});
         
